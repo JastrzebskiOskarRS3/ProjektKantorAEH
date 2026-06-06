3a2079d4e6da42bdcd2e111e66ae80c1298d951d
JastrzebskiOskarRS3 <ojtrades.rs3@gmail.com>
2026-06-06 08:08:20 +0200

    Update database.py

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
     password: str
-    balance_pln: float = Field(default=1000.0)
+    balance_pln: float = Field(default=0.0)
 
 class Transaction(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
